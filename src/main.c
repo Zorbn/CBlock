01da090d30a0610629e313ca4d74023aa36fbffa
Zorbn <public.zorb@gmail.com>
2023-07-14 14:13:35 -0700

    Basic player movement

--- src/main.c
+++ src/main.c
@@ -14,17 +14,42 @@
 #include <stdbool.h>
 #include <inttypes.h>
 
-// const float vertices[] = {
-//     0.5f, 0.5f, 0.0f, /* Color: */ 1.0f, 1.0f, 1.0f, /* Tex coords: */ 1.0f, 1.0f,   // Top right
-//     0.5f, -0.5f, 0.0f, /* Color: */ 1.0f, 1.0f, 1.0f, /* Tex coords: */ 1.0f, 0.0f,  // Bottom right
-//     -0.5f, -0.5f, 0.0f, /* Color: */ 1.0f, 1.0f, 1.0f, /* Tex coords: */ 0.0f, 0.0f, // Bottom left
-//     -0.5f, 0.5f, 0.0f, /* Color: */ 1.0f, 1.0f, 1.0f, /* Tex coords: */ 0.0f, 1.0f,  // Top left
-// };
+void move_camera(struct Window *window, float delta_time, vec3 eye) {
+    float forward_direction = 0.0f;
+    float up_direction = 0.0f;
+    float right_direction = 0.0f;
 
-// const uint32_t indices[] = {
-//     0, 1, 3, // Triangle 1
-//     1, 2, 3, // Triangle 2
-// };
+    if (glfwGetKey(window->glfw_window, GLFW_KEY_W) == GLFW_PRESS) {
+        forward_direction -= 1.0f;
+    }
+
+    if (glfwGetKey(window->glfw_window, GLFW_KEY_S) == GLFW_PRESS) {
+        forward_direction += 1.0f;
+    }
+
+    if (glfwGetKey(window->glfw_window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) {
+        up_direction -= 1.0f;
+    }
+
+    if (glfwGetKey(window->glfw_window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+        up_direction += 1.0f;
+    }
+
+    if (glfwGetKey(window->glfw_window, GLFW_KEY_A) == GLFW_PRESS) {
+        right_direction -= 1.0f;
+    }
+
+    if (glfwGetKey(window->glfw_window, GLFW_KEY_D) == GLFW_PRESS) {
+        right_direction += 1.0f;
+    }
+
+    const float camera_move_speed = 10.0f;
+    float current_move_speed = delta_time * camera_move_speed;
+
+    eye[0] += right_direction * current_move_speed;
+    eye[1] += up_direction * current_move_speed;
+    eye[2] += forward_direction * current_move_speed;
+}
 
 int main() {
     struct Window window = window_create("CBlock", 640, 480);
@@ -42,21 +67,31 @@ int main() {
     // TODO: Add mesh_destroy().
     struct Mesh mesh = mesher_mesh_chunk(&mesher, &chunk);
 
-    vec3 eye = {0.0f, 0.0f, -10.0f};
+    vec3 eye = {0.0f, 0.0f, 10.0f};
     vec3 center = {0.0f, 0.0f, 0.0f};
     vec3 up = {0.0f, 1.0f, 0.0f};
 
     mat4 view_matrix;
     mat4 projection_matrix;
-    glm_lookat(eye, center, up, view_matrix);
 
     int32_t view_matrix_location = glGetUniformLocation(program, "view_matrix");
     int32_t projection_matrix_location = glGetUniformLocation(program, "projection_matrix");
+
+    double last_time = glfwGetTime();
 
     while (!glfwWindowShouldClose(window.glfw_window)) {
         if (glfwGetKey(window.glfw_window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
             glfwSetWindowShouldClose(window.glfw_window, true);
         }
+
+        double current_time = glfwGetTime();
+        float delta_time = (float)(current_time - last_time);
+        last_time = current_time;
+
+        move_camera(&window, delta_time, eye);
+        glm_vec3_copy(eye, center);
+        center[2] -= 1.0f;
+        glm_lookat(eye, center, up, view_matrix);
 
         if (window.was_resized) {
             glViewport(0, 0, window.width, window.height);
